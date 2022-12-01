be57ddea007ed6c41c106c3e7215f03176cd43fe
SpellcasterNG <m19982607@gmail.com>
2022-12-01 11:43:42 -0400

    Se Genero ejecutable e instalable

--- QR_Generator_Contact/Properties/AssemblyInfo.cs
+++ QR_Generator_Contact/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("QR_Generator_Contact")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generador de QR VCart Contact")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SPellcaster")]
 [assembly: AssemblyProduct("QR_Generator_Contact")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Spellcaster")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
